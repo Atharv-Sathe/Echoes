655f41019ddae0823bf9202b939f09a87fee4012
AthSat <atharvsathe28704@gmail.com>
2024-12-20 19:33:44 +0530

    add middleware to handle error and an error handler function

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,23 +13,22 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required." });
+    next(errorHandler(400, "All fields are required."));
   }
 
   // In ES6 if the key and value are same you can just write username instead of username: username
   const hashedPass = bcryptjs.hashSync(password, 7);
- 
+
   const newUser = new User({
     username,
     email,
     password: hashedPass,
   });
 
-
   try {
     const success = await newUser.save();
     res.json({ message: "Success", success });
   } catch (error) {
-    res.json({ message: "Failure", error: error.message });
+    next(error);
   }
 };
--- server/index.js
+++ server/index.js
@@ -8,15 +8,14 @@ import authRoutes from "./routes/auth.route.js";
 
 // console.log(process.env.DATABASE_CONNECTION_URL)
 
-
 mongoose
-.connect(process.env.DATABASE_CONNECTION_URL)
-.then(() => {
+  .connect(process.env.DATABASE_CONNECTION_URL)
+  .then(() => {
     console.log("connection established");
-})
-.catch((res) => {
+  })
+  .catch((res) => {
     console.log("connection failed:" + res);
-});
+  });
 
 const app = express();
 
@@ -29,3 +28,13 @@ app.listen(5000, () => {
 app.use("/api/user", userRoutes);
 
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});

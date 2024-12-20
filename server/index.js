0b00f2a5ad89f9d9700bd63bc141e1d9d49aec9c
AthSat <atharvsathe28704@gmail.com>
2024-12-20 12:57:46 +0530

    connect mongodb cluster

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+// console.log(process.env.DATABASE_CONNECTION_URL)
+
+mongoose
+  .connect(process.env.DATABASE_CONNECTION_URL)
+  .then(() => {
+    console.log("connection established");
+  })
+  .catch((res) => {
+    console.log("connection failed:" + res);
+  });
 
 const app = express();
 

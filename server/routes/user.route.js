5c7804a45fb65f08627584d41000ba620a00fb9a
AthSat <atharvsathe28704@gmail.com>
2025-07-01 15:04:25 +0530

    Feat: Add signout user functionality and update routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, signoutUser, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -8,6 +8,8 @@ router.get("/test", test);
 
 router.put('/update/:userId', verifyToken, updateUser);
 
-router.delete('/delete/:userId', verifyToken, deleteUser)
+router.delete('/delete/:userId', verifyToken, deleteUser);
+
+router.post('/signout', signoutUser);
 
 export default router;

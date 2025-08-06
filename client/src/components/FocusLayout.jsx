3100380df5b204e87eb54eaf56d005de1d59fb51
AthSat <atharvsathe28704@gmail.com>
2025-08-06 23:12:33 +0530

    Feat: Replace HiViewGrid icon with IoExitOutline in FocusLayout component

--- client/src/components/FocusLayout.jsx
+++ client/src/components/FocusLayout.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { Link, Outlet } from "react-router-dom";
-import { HiViewGrid } from 'react-icons/hi';
+import { IoExitOutline } from "react-icons/io5";
 
 export default function FocusLayout() {
   return (
@@ -8,7 +8,7 @@ export default function FocusLayout() {
       {/* The "escape hatch" to the dashboard */}
       <Link to="/home">
         <div className="fixed top-4 right-4 z-50 p-2 rounded-full bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 transition-all">
-          <HiViewGrid className="w-6 h-6 text-gray-800 dark:text-gray-200" />
+          <IoExitOutline className="w-6 h-6 text-gray-800 dark:text-gray-200" />
         </div>
       </Link>
 
